1b8329364f5ab61cd68352e210bac132dfe7f7c2
Uncommon-20f <105292299+Uncommon-20f@users.noreply.github.com>
2024-05-24 16:31:05 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// const img = document.querySelector('.img')
+// img.classList.add('img1')
+
+// // console.log(img)
+
+// const searchcontainer = document.querySelector('.searchcontainer')
+// searchcontainer.classList.add('search')
+
+// const sidenav = document.querySelector('.sidenav')
+// sidenav.classList.add('side1')
